005fc115549f8bc537f10d260b2744c5e3c76d7b
sameerkhichi <khichis@mcmaster.ca>
2024-11-11 00:28:58 -0500

    patching header source file for correct function parameters

--- convert.h
+++ convert.h
@@ -1,3 +1,3 @@
 void print_usage(void);
 void print_help();
-int identify_args(int argc, char *argv[], int *base, long *start, long *finish, int *range_mode);
+int identify_args(int argc, char *argv[], int *base, long *start, long *finish);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     int base = 16;
     long start = 0, finish = 0;
 
-    if (identify_args(argc, argv, &base, &start, &finish, &range_mode) != 0) {
+    if (identify_args(argc, argv, &base, &start, &finish) != 0) {
         print_usage();
         exit(1);
     }
