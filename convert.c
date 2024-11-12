04d5926f98e5b688c5c7c38ccb8460dda40144e4
sameerkhichi <khichis@mcmaster.ca>
2024-11-11 23:12:19 -0500

    Added identification comments to C and header files

--- convert.c
+++ convert.c
@@ -1,3 +1,8 @@
+/*
+Name: Sameer Khichi
+MacID: khichis
+Student Number: 400518172
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- convert.h
+++ convert.h
@@ -1,3 +1,8 @@
+/*
+Name: Sameer Khichi
+MacID: khichis
+Student Number: 400518172
+*/
 void print_usage(void);
 void print_help();
 int identify_args(int argc, char *argv[], int *base, long *start, long *finish, int *range_mode);
